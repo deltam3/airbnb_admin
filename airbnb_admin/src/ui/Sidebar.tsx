51944be5fc86eb218bc2eb34a2dc224152a52bc1
deltam3 <practicect@proton.me>
2024-03-08 14:00:08 +0900

    created Sidebar component

---
+++ airbnb_admin/src/ui/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Sidebar = (props: Props) => {
+  return (
+    <div>
+      <h2>Sidebar</h2>
+    </div>
+  );
+};
+
+export default Sidebar;
