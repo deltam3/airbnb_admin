4ac64872712621680576f9a7ae1e6aa9d9b3b4b5
deltam3 <practicect@proton.me>
2024-03-08 14:53:01 +0900

    created and added Cabins page component to Router

--- airbnb_admin/src/App.tsx
+++ airbnb_admin/src/App.tsx
@@ -9,6 +9,7 @@ import Layout from "./ui/Layout";
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import Bookings from "./pages/Bookings";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           <Route index element={<Navigate replace to="dashboard" />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
---
+++ airbnb_admin/src/pages/Cabins.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cabins = () => {
+  return (
+    <div>
+      <h2>Cabins</h2>
+    </div>
+  );
+};
+
+export default Cabins;
