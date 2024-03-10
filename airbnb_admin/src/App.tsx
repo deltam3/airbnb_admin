e1e31b9677349a1dc78d49652d8aea5b4bd4c746
deltam3 <practicect@proton.me>
2024-03-10 09:30:21 +0900

    app.js fix routes

--- airbnb_admin/src/App.tsx
+++ airbnb_admin/src/App.tsx
@@ -5,6 +5,8 @@ import {
   Navigate,
 } from "react-router-dom";
 
+import GlobalStyles from "./styles/GlobalStyles";
+
 import Layout from "./ui/Layout";
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
@@ -17,21 +19,24 @@ import Account from "./pages/Account";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </Router>
+    <>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
