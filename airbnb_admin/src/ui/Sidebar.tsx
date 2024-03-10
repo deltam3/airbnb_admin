cb52d681b227e04b51abc6b478baa3fce5c00ffe
deltam3 <practicect@proton.me>
2024-03-10 10:20:59 +0900

    inital sidebar component styled

--- airbnb_admin/src/ui/Sidebar.tsx
+++ airbnb_admin/src/ui/Sidebar.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import styled from "styled-components";
 
-type Props = {};
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
-const Sidebar = (props: Props) => {
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+
+  grid-row: 1/-1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
+const Sidebar = () => {
   return (
-    <div>
-      <h2>Sidebar</h2>
-    </div>
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
   );
 };
 
