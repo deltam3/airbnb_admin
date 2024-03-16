a0acccd6c6d912d1e5bc41f2d297d709547f6746
deltam3 <practicect@proton.me>
2024-03-16 10:46:48 +0900

    created utils/helpers.js

--- airbnb_admin/src/features/rooms/RoomRow.tsx
+++ airbnb_admin/src/features/rooms/RoomRow.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
-type Props = {};
+function RoomRow({ room }) {
+  const { name, regularPrice, image, maxCapacity, discount } = room;
 
-const RoomRow = (props: Props) => {
-  return <div>RoomRow</div>;
-};
+  return (
+    <TableRow role="row">
+      <Img src={image}></Img>
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
+}
 
 export default RoomRow;
