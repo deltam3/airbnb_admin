5a346b947283cfc27645270bd524fd394e5231f1
deltam3 <practicect@proton.me>
2024-03-16 11:34:09 +0900

    added Container styled component in Layout component

--- airbnb_admin/src/ui/Layout.tsx
+++ airbnb_admin/src/ui/Layout.tsx
@@ -19,13 +19,23 @@ const Main = styled.div`
   gap: 3.2rem;
 `;
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function Layout() {
   return (
     <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
