e569745f37d5e35ee2669240afd9695cf7852c34
deltam3 <practicect@proton.me>
2024-03-16 10:33:14 +0900

    basic RoomTable setup

--- airbnb_admin/src/features/rooms/RoomTable.tsx
+++ airbnb_admin/src/features/rooms/RoomTable.tsx
@@ -4,11 +4,30 @@ import { getRooms } from "../../services/apiRooms";
 import RoomRow from "./RoomRow";
 type Props = {};
 
-const RoomTable = (props: Props) => {
+const RoomTable = () => {
+  const {
+    isLoading,
+    data: rooms,
+    error,
+  } = useQuery({
+    queryKey: ["rooms"],
+    queryFn: getRooms,
+  });
+
   return (
-    <div>
-      <h1>RoomTable</h1>
-    </div>
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Room</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {rooms.map((room) => {
+        <RoomRow room={room} key={room.id} />;
+      })}
+    </Table>
   );
 };
 
