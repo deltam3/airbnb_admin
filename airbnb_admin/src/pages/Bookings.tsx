df8cabeb879d6bd7c39618858b1efe9fe355bcc6
deltam3 <practicect@proton.me>
2024-03-24 14:31:01 +0900

    created BookingRow, BookingTable

--- airbnb_admin/src/pages/Bookings.tsx
+++ airbnb_admin/src/pages/Bookings.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import BookingTable from "../features/bookings/BookingTable";
 
 const Bookings = () => {
   return (
-    <div>
-      <h2>Bookings</h2>
-    </div>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All bookings</Heading>
+        <p>Test</p>
+      </Row>
+      <BookingTable></BookingTable>
+    </>
   );
 };
 
--- airbnb_admin/src/services/apiBookings.js
+++ airbnb_admin/src/services/apiBookings.js
@@ -5,7 +5,7 @@ export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, rooms(name), guests(fullName, email)",
       { count: "exact" }
     );
 
@@ -37,7 +37,7 @@ export async function getBookings({ filter, sortBy, page }) {
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
-    .select("*, cabins(*), guests(*)")
+    .select("*, rooms(*), guests(*)")
     .eq("id", id)
     .single();
 
@@ -46,5 +46,76 @@ export async function getBooking(id) {
     throw new Error("Booking not found");
   }
 
+  return data;
+}
+
+export async function getBookingsAfterDate(date) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("created_at, totalPrice, extrasPrice")
+    .gte("created_at", date)
+    .lte("created_at", getToday({ end: true }));
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+
+  return data;
+}
+
+export async function getStaysAfterDate(date) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, guests(fullName)")
+    .gte("startDate", date)
+    .lte("startDate", getToday());
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+
+  return data;
+}
+
+export async function getStaysTodayActivity() {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, guests(fullName, nationality, countryFlag)")
+    .or(
+      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
+    )
+    .order("created_at");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+  return data;
+}
+
+export async function updateBooking(id, obj) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .update(obj)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be updated");
+  }
+  return data;
+}
+
+export async function deleteBooking(id) {
+  const { data, error } = await supabase.from("bookings").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be deleted");
+  }
   return data;
 }
